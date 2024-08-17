75ab985cdcc83d7dda26b255237cd6613385510c
ridwanfauze26 <145428228+ridwanfauze26@users.noreply.github.com>
2024-08-17 23:11:39 +0700

    Add files via upload

---
+++ Pertemuan7/src/main/java/com/mycompany/pertemuan7/Pertemuan7.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan7;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pertemuan7 {
+
+    public static void main(String[] args) {
+        int day = 1;
+        
+    switch (day) {
+        case 1:
+            System.out.println("Sabtu");
+            
+        case 2:
+            System.out.println("Minggu");
+         
+        default:
+            System.out.println("Hari tidak valid");
+            break;
+ }
+       }
+}
